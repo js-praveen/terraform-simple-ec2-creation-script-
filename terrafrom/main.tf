6ebb5a236f2c6af2b4596b31742935bec3c9faa8
js-praveen <antonypraveen90@gmail.com>
2026-02-21 17:52:08 +0530

    Add files via upload

---
+++ terrafrom/main.tf
@@ -0,0 +1,21 @@
+ provider "aws" {
+   region = "us-west-2"
+ }
+ data "aws_ami" "ubuntu" {
+   most_recent = true
+
+   filter {
+     name = "name"
+     values = ["ubuntu/image/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+   }
+   owners = ["099720109477"] #this ubuntu offical
+ }
+
+   resource "aws_instance" "ec2_name" {
+     ami           = data.aws_ami.ubuntu.id
+     instance_type = "t2.micro"
+
+     tags = {
+       Name = "learn-terraform"
+     }
+   }
